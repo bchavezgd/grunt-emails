40cc32626b702f8f584ce5a03fd6ac622649f015
nccpt-git <FUTUREFIT12-MAC@The-Mountain.local>
2015-07-31 11:20:39 -0700

    initial commit

---
+++ src/helpers/each.js
@@ -0,0 +1,9 @@
+Handlebars.registerHelper('each', function(context, options) {
+  var ret = "";
+
+  for(var i=0, j=context.length; i<j; i++) {
+    ret = ret + options.fn(context[i]);
+  }
+
+  return ret;
+});
