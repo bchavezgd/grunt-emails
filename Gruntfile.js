6b4e195862b0f672a9c9ff63b6a79a82be571649
nccpt-git <FUTUREFIT12-MAC@The-Mountain.home>
2016-02-01 11:49:58 -0800

    able to move forward

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,216 +1,191 @@
 module.exports = function (grunt) {
 
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
-        // secrets.json is ignored in git because it contains sensitive data
-        //secrets: grunt.file.readJSON('secrets.json'),
-
-
-        // Re-usable filesystem paths
-        paths: {
-          src: './src',
-          src_img: './src/img',
-          dist: './dist',
-          dist_img: './dist/img'
-        },
-
-        // Takes your scss files and compiles them to css
+    // secrets.json is ignored in git because it contains sensitive data
+    //secrets: grunt.file.readJSON('secrets.json'),
 
 
-        sass: {
-          options: {
-            style: 'expanded',
-            sourceComments: true
-          },
-          file: {
-            src: '<%= paths.src %>/scss/nccpt.scss',
-            dest: '<%= paths.dist %>/css/nccpt.css'
-          }
-        },
+    // Re-usable filesystem paths
+    paths: {
+      src: './src',
+      src_img: './src/img',
+      dist: './dist',
+      dist_img: './dist/img'
+    },
+
+    // Takes your scss files and compiles them to css
 
 
-        postcss: {
-          options: {
-            processors: [
+    sass: {
+      options: {
+        style: 'expanded',
+        sourceComments: true
+      },
+      file: {
+        src: '<%= paths.src %>/scss/nccpt.scss',
+        dest: '<%= paths.dist %>/css/nccpt.css'
+      }
+    },
+
+
+    postcss: {
+      options: {
+        processors: [
 					//	add fallbacks for rem units
         	require('pixrem')(),
         	//	add vendor prefixes
 					require('autoprefixer')({
-                browsers: 'last 5 versions'
-              }),
+            browsers: 'last 5 versions'
+          }),
 				]
-          },
-          file: {
-            src: '<%= paths.dist %>/css/nccpt.css',
-            dest: '<%= paths.dist %>/css/nccpt.css'
-          }
-        },
-        juice: {
-          // grunt-inline-css
-          options: {
-            webResources: {
-              applyStyleTags: false,
-              removeStyleTags: false,
-              preserveMediaQueries: false,
-              applyAttributesTableElements: false,
-              applyWidthAttributes: true,
-              preserveImportant: true,
-              preserveFontFaces: false,
-              insertPreservedExtraCss: true,
-//              xmlMode: true,
-              webResources: {
-                images: false,
-                links: true
-              }
-            }},
-            file: {
-              src: './dist/160205-nccpt.html',
-              dest: './dist/juiced/160205-nccpt.html'
-            }
-          
-          },
+      },
+      file: {
+        src: '<%= paths.dist %>/css/nccpt.css',
+        dest: '<%= paths.dist %>/css/nccpt.css'
+      }
+    },
 
-          // Assembles your email content with html layout
-          assemble: {
-            options: {
-              layoutext: '.hbs',
-              layout: 'nccpt',
-              plugins: ['assemble-markdown-data'],
-              layoutdir: '<%= paths.src %>/layouts',
-              partials: ['<%= paths.src %>/partials/**/*.hbs'],
-              data: '<%= paths.src %>/data/*.{json,yml}',
-              flatten: true
-            },
-            pages: {
-              expand: true,
-              cwd: '<%= paths.src %>/emails/',
-              src: ['*.{hbs,md}'],
-              dest: '<%= paths.dist %>/'
-            }
-          },
+    // Assembles your email content with html layout
+    assemble: {
+      options: {
+        layoutext: '.hbs',
+        layout: 'nccpt',
+        plugins: ['assemble-markdown-data'],
+        layoutdir: '<%= paths.src %>/layouts',
+        partials: ['<%= paths.src %>/partials/**/*.hbs'],
+        data: '<%= paths.src %>/data/*.{json,yml}',
+        flatten: true
+      },
+      pages: {
+        expand: true,
+        cwd: '<%= paths.src %>/emails/',
+        src: ['*.{hbs,md}'],
+        dest: '<%= paths.dist %>/'
+      }
+    },
 
 
 
 
 
-          // Replace compiled template images sources from ../src/html to ../dist/html
-          replace: {
-            src_images: {
-              options: {
-                usePrefix: false,
-                patterns: [{
-                  match: /(<img[^>]+[\"'])(\.\.\/src\/img\/)/gi, // Matches <img * src="../src/img or <img * src='../src/img'
-                  replacement: '$1../<%= paths.dist_img %>/'
+    // Replace compiled template images sources from ../src/html to ../dist/html
+    replace: {
+      src_images: {
+        options: {
+          usePrefix: false,
+          patterns: [{
+            match: /(<img[^>]+[\"'])(\.\.\/src\/img\/)/gi, // Matches <img * src="../src/img or <img * src='../src/img'
+            replacement: '$1../<%= paths.dist_img %>/'
           }, {
-                  match: /(url\(*[^)])(\.\.\/src\/img\/)/gi, // Matches url('../src/img') or url(../src/img) and even url("../src/img")
-                  replacement: '$1../<%= paths.dist_img %>/'
+            match: /(url\(*[^)])(\.\.\/src\/img\/)/gi, // Matches url('../src/img') or url(../src/img) and even url("../src/img")
+            replacement: '$1../<%= paths.dist_img %>/'
           }]
-              },
-              files: [{
-                expand: true,
-                flatten: true,
-                src: ['<%= paths.dist %>/*.html'],
-                dest: '<%= paths.dist %>'
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['<%= paths.dist %>/*.html'],
+          dest: '<%= paths.dist %>'
         }]
-            }
-          },
+      }
+    },
 
-          // Inlines your css
-          premailer: {
-            html: {
-              options: {
-                removeComments: true
-              },
-              files: [{
-                expand: true,
-                src: ['<%= paths.dist %>/*.html'],
-                dest: ''
+    // Inlines your css
+    premailer: {
+      html: {
+        options: {
+          removeComments: true
+        },
+        files: [{
+          expand: true,
+          src: ['<%= paths.dist %>/*.html'],
+          dest: ''
         }]
-            },
-            txt: {
-              options: {
-                mode: 'txt'
-              },
-              files: [{
-                expand: true,
-                src: ['<%= paths.dist %>/*.html'],
-                dest: '',
-                ext: '.txt'
+      },
+      txt: {
+        options: {
+          mode: 'txt'
+        },
+        files: [{
+          expand: true,
+          src: ['<%= paths.dist %>/*.html'],
+          dest: '',
+          ext: '.txt'
 				}]
-            }
-          },
+      }
+    },
 
-          // Optimize images
-          imagemin: {
-            dynamic: {
-              options: {
-                optimizationLevel: 0,
-                svgoPlugins: [{
-                  removeViewBox: false
+    // Optimize images
+    imagemin: {
+      dynamic: {
+        options: {
+          optimizationLevel: 0,
+          svgoPlugins: [{
+            removeViewBox: false
 					}]
-              },
-              files: [{
-                expand: true,
-                cwd: '<%= paths.src_img %>',
-                src: ['**/*.{png,jpg,gif}'],
-                dest: '<%= paths.dist_img %>'
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= paths.src_img %>',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: '<%= paths.dist_img %>'
        }]
-            }
-          },
+      }
+    },
 
-          // setting up dev server with livereload
-          connect: {
-            server: {
-              options: {
-                hostname: 'localhost',
-                port: '3300',
-                livereload: true,
-                base: '<%= paths.dist %>'
-              }
-            }
-          },
+    // setting up dev server with livereload
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+          port: '3300',
+          livereload: true,
+          base: '<%= paths.dist %>'
+        }
+      }
+    },
 
-          // Watches for changes to source files
-          watch: {
-            files: [
+    // Watches for changes to source files
+    watch: {
+      files: [
 				'<%= paths.src %>/scss/*',
 				'<%= paths.src %>/emails/*',
 				'<%= paths.src %>/layouts/*',
 				'<%= paths.src %>/partials/*',
 				'<%= paths.src %>/data/*'
 			],
-            tasks: [
+      tasks: [
 				'sass',
 				'postcss',
 				'assemble',
 				'newer:imagemin'
 			],
-            options: {
-              spawn: false,
-              event: 'all',
-              livereload: true
-            }
-          }
-        });
+      options: {
+        spawn: false,
+        event: 'all',
+        livereload: true
+      }
+    }
+  });
 
-      // Load all grunt-* tasks
-      require('load-grunt-tasks')(grunt);
+  // Load all grunt-* tasks
+  require('load-grunt-tasks')(grunt);
 
-      // registering grunt tasks.
-      grunt.registerTask('done', [
+  // registering grunt tasks.
+  grunt.registerTask('done', [
 		'sass',
 		'postcss',
 		'assemble',
 		'premailer',
 		'newer:imagemin',
 		'newer:replace:src_images'
-    //'juice'
 	]);
 
-      grunt.registerTask('default', [
+  grunt.registerTask('default', [
 		'sass',
 		'postcss',
 		'assemble',
@@ -218,4 +193,4 @@ module.exports = function (grunt) {
 		'connect',
 		'watch'
 	]);
-    };
+};
