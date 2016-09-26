5d695382f05cbd1855e2702d974fadf78b23a1ef
nccpt-git <FUTUREFIT12-MAC@The-Mountain.home>
2016-09-26 10:03:40 -0700

    gh-pages failed me.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
       pkg: grunt.file.readJSON('package.json'),
       // Re-usable filesystem paths
       paths: {
-        site: './_site/2016',
+        site: './docs/2016',
         src: './src',
         src_img: './src/img',
         dist: './dist',
